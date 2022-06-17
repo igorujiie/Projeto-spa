2f69a4a368ab158887599746ff49e807339f122a
igor ujiie <igorkazuhico@hotmail.com>
2022-06-17 18:51:17 -0300

    Final Aula 2

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -42,14 +42,15 @@ export function WidegtForm() {
     const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestartFeedback() {
-        setFeedbackType(null)
+        setFeedbackSent(false);
+        setFeedbackType(null);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
             {feedbackSent ? (
-                <FeedbackSucessStep />
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
             ) : (
                 <>
                     {!feedbackType ? (
