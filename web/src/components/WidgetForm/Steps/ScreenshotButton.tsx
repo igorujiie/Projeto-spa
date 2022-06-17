8c7f2a330d9417b78ea84d9ac54348f5d1739133
igor ujiie <igorkazuhico@hotmail.com>
2022-06-17 17:15:38 -0300

    loading do screenshot

--- web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
@@ -1,11 +1,15 @@
 import { Camera } from "phosphor-react";
 import html2canvas from 'html2canvas'
+import { useState } from "react";
+import { Loading } from "../Loading";
 
 export function ScreenshotButton(){
+    const[isTakingScreenshot, setIsTakingScreenshot] = useState(false)
     async function handleTakeScreenShot(){
+        setIsTakingScreenshot(true);
         const canvas = await html2canvas(document.querySelector('html')!);
         const base64image = canvas.toDataURL('image/png')
-
+        setIsTakingScreenshot(false);
     }
     return(
         <button 
@@ -13,7 +17,7 @@ export function ScreenshotButton(){
         onClick={handleTakeScreenShot}
         className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
         >
-        <Camera  className="w-6 h-6  "/>        
+        {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" />}       
         </button>
     )
 }
