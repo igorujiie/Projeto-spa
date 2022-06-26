ae0a1e474a837aa7950761bff3a5db0559e7064c
igor ujiie <igorkazuhico@hotmail.com>
2022-06-26 11:43:06 -0300

    fazendo o componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import { Widget } from "./components/Widget";
-
+import {APIcovid} from "./components/APIcovid";
 export function App(){
   return ( <div>
     <Widget />
+    <APIcovid />
   </div>)
 }
---
+++ web/src/components/APIcovid/index.tsx
@@ -0,0 +1,40 @@
+export function APIcovid(){
+    return(
+      
+
+
+    
+  <>
+  <body>
+    <header className="header">
+    
+      <h2>Dados por regiao da Covid-19</h2>
+    </header>
+
+    <div className="container">
+      <form className="my-form">
+        <label id="label" for="cep">Digite um CEP: </label>
+        <input id="input-cep" type="text" data-js="cep" />
+        <button className="button" id="button">Pesquisar</button>
+      </form>
+
+      <div className="output">
+        <p>Estado: <span id="span_estado"></span></p>
+        <p>Casos: <span id="span_casos"></span></p>
+        <p>Mortes: <span id="span_mortes"></span></p>
+        <p>Suspeitos: <span id="span_suspeitos"></span></p>
+        <p>Recusados: <span id="span_recusados"></span></p>
+        <p id="update">Última atualização: <span id="span_atualizacao"></span></p>
+      </div>
+    </div>
+
+    <footer className="footer">
+      <p> Igo Kazuhiko Ujiie - Developer </p>
+    </footer>
+
+    <script type="module" src="./js/main.js"></script>
+  </body>
+  </>
+
+    )
+}
