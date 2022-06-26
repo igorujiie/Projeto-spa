3bc8d990f58bce13343c24620f2d9468f4fbcab4
igor ujiie <igorkazuhico@hotmail.com>
2022-06-26 17:30:23 -0300

    arrumando o Botao

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,15 @@
 import { Widget } from "./components/Widget";
-import {APIcovid} from "./components/APIcovid";
+import { APIcovid } from "./components/APIcovid";
 import { createContext, useState } from "react";
-import ReactSwitch from "react-switch";
-
 
 export const ThemeContext = createContext(null);
 
-export function App(){
-
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  };
+export function App() {
 
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-      <div className="App" id={theme}> 
-        <Widget />
-        <div className="switch">
-          <label > {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-          <ReactSwitch onChange={toggleTheme} checked={theme ==="dark"} />
-        </div>
-        <APIcovid />
-      </div>
-    </ThemeContext.Provider>
+    <>
+    <Widget />
+    <APIcovid />
+    </>
   )
 }
--- web/src/components/APIcovid/index.tsx
+++ web/src/components/APIcovid/index.tsx
@@ -1,37 +1,52 @@
 import ReactSwitch from "react-switch";
+import { createContext, useState } from "react";
+import { ThemeContext } from "../../App";
 
 export function APIcovid() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-
-
     <>
       <body>
-        <header className="header">
 
-          <h2>Dados por regiao da Covid-19</h2>
-        </header>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <div className="App" id={theme}>
+            <header className="header">
 
-        <div className="container">
-          <form className="my-form">
-            <label id="label" htmlFor="cep">Digite um CEP: </label>
-            <input id="input-cep" type="text" data-js="cep" />
-            <button className="button" id="button">Pesquisar</button>
-          </form>
+              <h2>Dados por regiao da Covid-19</h2>
+            </header>
 
-          <div className="output">
-            <p>Estado: <span id="span_estado"></span></p>
-            <p>Casos: <span id="span_casos"></span></p>
-            <p>Mortes: <span id="span_mortes"></span></p>
-            <p>Suspeitos: <span id="span_suspeitos"></span></p>
-            <p>Recusados: <span id="span_recusados"></span></p>
-            <p id="update">Última atualização: <span id="span_atualizacao"></span></p>
+            <div className="container">
+              <form className="my-form">
+                <label id="label" htmlFor="cep">Digite um CEP: </label>
+                <input id="input-cep" type="text" data-js="cep" />
+                <button className="button" id="button">Pesquisar</button>
+              </form>
+
+              <div className="output">
+                <p>Estado: <span id="span_estado"></span></p>
+                <p>Casos: <span id="span_casos"></span></p>
+                <p>Mortes: <span id="span_mortes"></span></p>
+                <p>Suspeitos: <span id="span_suspeitos"></span></p>
+                <p>Recusados: <span id="span_recusados"></span></p>
+                <p id="update">Última atualização: <span id="span_atualizacao"></span></p>
+              </div>
+            </div>
+
+            <div className="switch">
+              <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+            </div>
+
+            <footer className="footer">
+              <p> Igo Kazuhiko Ujiie - Developer </p>
+            </footer>
           </div>
-        </div>
-        
-        <footer className="footer">
-          <p> Igo Kazuhiko Ujiie - Developer </p>
-        </footer>
-
+        </ThemeContext.Provider>
         <script type="module" src="./modules/main.js"></script>
       </body>
     </>
